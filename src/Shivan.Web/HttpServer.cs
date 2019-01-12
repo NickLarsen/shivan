daf591a3d93f95c0cc45b8a63499c6069238ed77
Nick Larsen <fodylarsen@gmail.com>
2019-01-11 20:47:49 -0500

    just getting started

---
+++ src/Shivan.Web/HttpServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Shivan.Web
+{
+    public class HttpServer
+    {
+        public void Start()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5431);
+
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try{
+                listener.Bind(localEndPoint);
+                listener.Listen(100);
+
+                while(true) {
+                    //https://docs.microsoft.com/en-us/dotnet/framework/network-programming/asynchronous-server-socket-example
+                }
+            }
+        }
+    }
+}
